31022517b6446e8971aeb9c7cea551e7eb6289a2
stas <stas@94b062d5-acec-0310-b7ce-b19ef95377eb>
2005-02-27 06:21:50 +0000

    apxs/mod_scoreboard_send.c has been ported to Apache 2.0

--- apxs/mod_scoreboard_send.c
+++ apxs/mod_scoreboard_send.c
@@ -1,40 +1,24 @@
-#include "httpd.h"  
-#include "http_config.h"  
-#include "http_request.h"  
-#include "http_protocol.h"  
-#include "http_core.h"  
-#include "http_main.h" 
-#include "http_log.h"  
-
+#include "httpd.h"
+#include "http_config.h"
+#include "http_protocol.h"
+#include "http_request.h"
+#include "ap_mpm.h"
 #include "scoreboard.h"
-
 #include "send.c"
 
-static handler_rec handlers[] =
+static void register_hooks(apr_pool_t *p)
 {
-    {"scoreboard-send-handler", scoreboard_send},
-    {NULL}
-};
+    ap_hook_handler(scoreboard_send, NULL, NULL, APR_HOOK_MIDDLE);
+}
 
-module MODULE_VAR_EXPORT scoreboard_send_module = {
-    STANDARD_MODULE_STUFF, 
-    NULL,                  /* module initializer                  */
-    NULL,                  /* create per-dir    config structures */
-    NULL,                  /* merge  per-dir    config structures */
-    NULL,                  /* create per-server config structures */
-    NULL,                  /* merge  per-server config structures */
-    NULL,                  /* table of config file commands       */
-    handlers,              /* [#8] MIME-typed-dispatched handlers */
-    NULL,                  /* [#1] URI to filename translation    */
-    NULL,                  /* [#4] validate user id from request  */
-    NULL,                  /* [#5] check if the user is ok _here_ */
-    NULL,                  /* [#3] check access by host address   */
-    NULL,                  /* [#6] determine MIME type            */
-    NULL,                  /* [#7] pre-run fixups                 */
-    NULL,                  /* [#9] log a transaction              */
-    NULL,                  /* [#2] header parser                  */
-    NULL,                  /* child_init                          */
-    NULL,                  /* child_exit                          */
-    NULL                   /* [#0] post read-request              */
+module AP_MODULE_DECLARE_DATA scoreboard_send_module =
+{
+    STANDARD20_MODULE_STUFF,
+    NULL,    /* per-directory config creator */
+    NULL,     /* dir config merger */
+    NULL, /* server config creator */
+    NULL,  /* server config merger */
+    NULL,                 /* command table */
+    register_hooks,       /* set up other request processing hooks */
 };
 
