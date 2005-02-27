4e7a08b7287af2408f1595f058efa2c273620f52
stas <stas@94b062d5-acec-0310-b7ce-b19ef95377eb>
2005-02-27 06:26:09 +0000

    add some docs

--- apxs/mod_scoreboard_send.c
+++ apxs/mod_scoreboard_send.c
@@ -1,3 +1,20 @@
+/*
+
+Description:
+
+This module sends an Apache 2 scoreboard image as a response. This
+image can be parsed by Apache::Scoreboard perl module.
+
+Usage:
+
+  LoadModule scoreboard_send_module path/to/mod_scoreboard_send.so
+  
+  <Location /scoreboard>
+     SetHandler scoreboard-send-handler
+  </Location>
+
+*/
+
 #include "httpd.h"
 #include "http_config.h"
 #include "http_protocol.h"
@@ -14,11 +31,11 @@ static void register_hooks(apr_pool_t *p)
 module AP_MODULE_DECLARE_DATA scoreboard_send_module =
 {
     STANDARD20_MODULE_STUFF,
-    NULL,    /* per-directory config creator */
-    NULL,     /* dir config merger */
-    NULL, /* server config creator */
-    NULL,  /* server config merger */
-    NULL,                 /* command table */
-    register_hooks,       /* set up other request processing hooks */
+    NULL,            /* per-directory config creator */
+    NULL,            /* dir config merger */
+    NULL,            /* server config creator */
+    NULL,            /* server config merger */
+    NULL,            /* command table */
+    register_hooks,  /* set up other request processing hooks */
 };
 
